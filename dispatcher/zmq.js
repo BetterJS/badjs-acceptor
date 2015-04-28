1e65fdfedfb2b59dbe8593d10a30b01b26cdc9cb
Chris Cai <caihuijigood@gmail.com>
2015-04-28 13:30:00 +0800

    修改日志

--- dispatcher/zmq.js
+++ dispatcher/zmq.js
@@ -4,6 +4,9 @@ var map = require('map-stream')
   , port =  GLOBAL.pjconfig.zmq.url
   , service =  GLOBAL.pjconfig.zmq.subscribe;
 
+var log4js = require('log4js'),
+    logger = log4js.getLogger();
+
 client.bind(port, function (err) {
   if (err) throw err;
 });
@@ -17,7 +20,7 @@ module.exports = function () {
   var stream = map(function (data, fn) {
     client.send(service + ' ' +  JSON.stringify(data.data));
 
-      global.logger.debug('dispatcher a message : ' + 'badjs' + ' ' +  JSON.stringify(data.data))
+      logger.debug('dispatcher a message : ' + 'badjs' + ' ' +  JSON.stringify(data.data))
   });
   return stream;
 };
