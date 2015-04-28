74acdf96a0358df6a010c337a51a478b95ca6130
Chris Cai <caihuijigood@gmail.com>
2015-04-28 13:29:12 +0800

    修改日志

--- app.js
+++ app.js
@@ -10,12 +10,11 @@ var argv = process.argv.slice(2);
 
 if(argv.indexOf('--debug') >= 0){
     logger.setLevel('DEBUG');
-    logger.debug("running in debug");
+    global.debug = true;
 }else {
     logger.setLevel('INFO');
 }
 
-global.logger = logger;
 
 if(argv.indexOf('--project') >= 0){
     GLOBAL.pjconfig =  require('./project.debug.json')
@@ -44,7 +43,13 @@ global.projectsId = '';
 connect()
   .use('/badjs', connect.query())
   .use('/badjs', function (req, res) {
-    global.logger.debug('===== get a message =====');
+
+    if( global.debug){
+        logger.setLevel('DEBUG');
+    }
+
+
+    logger.debug('===== get a message =====');
 
     var id ;
     if( isNaN(( id = req.query.id - 0) ) || id <=0 ||id >= 9999 || global.projectsId.indexOf(id)<0){
@@ -54,7 +59,7 @@ connect()
         });
         res.statusCode = 403;
         res.write("forbidden " );
-        global.logger.debug("forbidden :" + req.query.id);
+        logger.debug("forbidden :" + req.query.id);
         res.end();
         return ;
     }
@@ -72,7 +77,7 @@ connect()
         res.statusCode = 403;
         res.write("forbidden" );
 
-        global.logger.info("parse param  error :" + e);
+        logger.info("parse param  error :" + e);
         res.end();
         return ;
     }
@@ -83,12 +88,12 @@ connect()
     });
     res.statusCode = 204;
 
-    global.logger.debug("===== complete a message =====");
+    logger.debug("===== complete a message =====");
     res.end();
   })
   .listen(80);
 
-global.logger.info('start badjs-accepter , listen 80 ...');
+logger.info('start badjs-accepter , listen 80 ...');
 
 
 setTimeout(function (){
