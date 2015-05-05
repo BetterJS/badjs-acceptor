208bae4ba3ce457dbd74ce3625f634908bbb338c
Chris Cai <caihuijigood@gmail.com>
2015-05-05 18:26:36 +0800

    当获取ip 错误的时候，返回默认IP

--- filter/addExtStream.js
+++ filter/addExtStream.js
@@ -8,10 +8,15 @@ var map = require('map-stream');
 module.exports = function (nextStream) {
 
     function getClientIp(req) {
-        return req.headers['x-forwarded-for'] ||
-            req.connection.remoteAddress ||
-            req.socket.remoteAddress ||
-            req.connection.socket.remoteAddress;
+        try{
+            return req.headers['x-forwarded-for'] ||
+                req.connection.remoteAddress ||
+                req.socket.remoteAddress ||
+                req.connection.socket.remoteAddress;
+        }catch(e){
+
+        }
+        return "0.0.0.0";
     };
 
     var stream = map(function (data, fn) {
