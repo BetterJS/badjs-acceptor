cef71adda40e685e8a335df06ee1c46e08133945
yorts52 <yorts52@qq.com>
2015-08-14 22:28:07 +0800

    format and add access-control-allow-origin

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
-'use strict';
+/* global process, global, GLOBAL */
+var connect = require('connect'),
+    log4js = require('log4js'),
+    logger = log4js.getLogger();
 
-var connect = require('connect')
-    , log4js = require('log4js'),
-     logger = log4js.getLogger();
-
-var cluster = require("cluster");
+var cluster = require('cluster');
 var argv = process.argv.slice(2);
 
-
-if(argv.indexOf('--debug') >= 0){
+if (argv.indexOf('--debug') >= 0) {
     logger.setLevel('DEBUG');
     global.debug = true;
-}else {
+} else {
     logger.setLevel('INFO');
 }
 
-
-if(argv.indexOf('--project') >= 0){
-    GLOBAL.pjconfig =  require('./project.debug.json')
-}else {
-    GLOBAL.pjconfig =  require('./project.json');
+if (argv.indexOf('--project') >= 0) {
+    GLOBAL.pjconfig = require('./project.debug.json');
+} else {
+    GLOBAL.pjconfig = require('./project.json');
 }
 
-
-
 if (cluster.isMaster) {
-
 
     var clusters = [];
     // Fork workers.
@@ -34,96 +28,82 @@ if (cluster.isMaster) {
         clusters.push(forkCluster);
     }
 
-    setTimeout(function (){
+    setTimeout(function() {
         require('./service/ProjectService')(clusters);
-    },1000)
+    }, 1000);
 
-
-    return ;
+    return;
 }
 
-
-var interceptor = require("c-interceptor")();
+var interceptor = require('c-interceptor')();
 var interceptors = GLOBAL.pjconfig.interceptors;
 
-
-interceptors.forEach(function (value ,key){
+interceptors.forEach(function(value, key) {
     var one = require(value)();
     interceptor.add(one);
 });
 interceptor.add(require(GLOBAL.pjconfig.dispatcher.module)());
 
-var forbiddenData = "forbidden";
+var forbiddenData = '403 forbidden';
 
 global.projectsId = '';
-process.on("message" , function (data){
+
+process.on('message', function(data) {
     var json = data;
-    if(json.projectsId){
+    if (json.projectsId) {
         global.projectsId = json.projectsId;
     }
-
-})
-
+});
 
 connect()
-  .use('/badjs', connect.query())
-  .use('/badjs', function (req, res) {
+    .use('/badjs', connect.query())
+    .use('/badjs', function(req, res) {
 
+        logger.debug('===== get a message =====');
 
-    logger.debug('===== get a message =====');
-
-    var id ;
-    if( isNaN(( id = req.query.id - 0) ) || id <=0 ||id >= 9999 || global.projectsId.indexOf(id)<0){
-
-
-        res.writeHead(403, {
+        var responseHeader = {
+            'Access-Control-Allow-Origin': '*',
             'Content-Type': 'image/jpeg',
-            'Content-length': forbiddenData.length,
-            "Connection": "close"
-        });
-        res.statusCode = 403;
-        res.write(forbiddenData );
-        logger.debug("forbidden :" + req.query.id);
+            'Connection': 'close'
+        };
+
+        var id = req.query.id - 0;
+        if (isNaN(id) ||
+            id <= 0 ||
+            id >= 9999 ||
+            global.projectsId.split('_').indexOf(id) < 0) {
+
+            responseHeader['Content-length'] = forbiddenData.length;
+            res.writeHead(403, responseHeader);
+            res.write(forbiddenData);
+            logger.debug('forbidden :' + req.query.id);
+            res.end();
+            return;
+        }
+
+        req.query.id = id;
+
+        try {
+            interceptor.invoke({
+                req: req,
+                data: req.query
+            });
+        } catch (err) {
+            responseHeader['Content-length'] = forbiddenData.length;
+            res.writeHead(403, responseHeader);
+            res.write(forbiddenData);
+            logger.info('parse param  error :' + err);
+            res.end();
+            return;
+        }
+
+        // responseHeader end with 204
+        responseHeader['Content-length'] = 0;
+        res.writeHead(204, responseHeader);
+
+        logger.debug('===== complete a message =====');
         res.end();
-        return ;
-    }
+    })
+    .listen(GLOBAL.pjconfig.port);
 
-    req.query.id = id;
-
-
-    try{
-        interceptor.invoke({req : req , data : req.query});
-
-    }catch(e) {
-        res.writeHead(403, {
-            'Content-Type': 'image/jpeg',
-            'Content-length': forbiddenData.length,
-            "Connection": "close"
-        });
-        res.statusCode = 403;
-        res.write(forbiddenData );
-
-        logger.info("parse param  error :" + e);
-        res.end();
-        return ;
-    }
-
-    // response end with 204
-    res.writeHead(204, {
-        'Content-Type': 'image/jpeg',
-        "Content-length": 0
-    });
-    res.statusCode = 204;
-
-    logger.debug("===== complete a message =====");
-    res.end();
-  })
-  .listen(  GLOBAL.pjconfig.port );
-  //.listen({port: GLOBAL.pjconfig.port , backlog :1024000});
-
-logger.info('start badjs-accepter , listen '+GLOBAL.pjconfig.port+' ...');
-
-
-
-
-
+logger.info('start badjs-accepter , listen ' + GLOBAL.pjconfig.port + ' ...');
